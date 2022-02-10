f6f3583ec0be5f3442044abcaa372cb4405652ef
BryanGonzalez <bgonzalezp02@educantabria.es>
2022-02-10 03:18:36 +0100

    Intento numero 3000 de hacer comit

--- grupo3/src/components/perfil/Perfil.jsx
+++ grupo3/src/components/perfil/Perfil.jsx
@@ -21,76 +21,109 @@ class Perfil extends React.Component {
   }
 
   render() {
-    const IhandlePerfilSetting = (valorA, valorB) =>{
-        console.log(valorA, valorB)
-        if (
-            valorA !== undefined &&
-            valorA !== "" &&
-            valorA !== this.state.valor
-          ) {
-            console.log("Cambiando de email");
-            this.setState({ valorB: valorA });
-          } else {
-            switch (valorA) {
-              case "":
-                console.log("El email no puede estar vacio.");
-                break;
-              case undefined:
-                console.log("El email no puede estar indefinido.");
-                break;
-              default:
-                console.log("El email no puede ser el mismo.");
-                break;
-            }
-          }
-    }
+    const handleChangeEmail = () => {
+      this.setState({ email: this.inputEmail.current.value });
+    };
 
-    const handlePerfilSetting = () => {
-        IhandlePerfilSetting(this.inputEmail.current.value, this.state.email);
+    const handleChangeUser = () => {
+      this.setState({ user: this.inputUser.current.value });
+    };
+
+    const handleChangePassword = () => {
+      this.setState({ password: this.inputPassword.current.value });
+    };
+
+    const handleChangeImg = () => {
+      this.setState({ img: this.inputImg.current.value });
     };
 
     return (
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address: {this.state.email}</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Introduzca su email para actualizar."
-            ref={this.inputEmail}
-          />
-        </Form.Group>
+      <div>
+        <div>
+          <h2>Bienvenido! {this.state.user || this.state.email}</h2>
+        </div>
+        <div>
+          <div>
+            <strong>Su configuracion es la siguiente: </strong>
+          </div>
+          <div className="mb-3">
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address: {this.state.email}</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Introduzca su email para actualizar."
+                  ref={this.inputEmail}
+                />
+                <Button
+                  variant="primary"
+                  type="button"
+                  onClick={handleChangeEmail}
+                >
+                  Cambiar
+                </Button>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicText">
-          <Form.Label>User name: {this.state.user}</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Introduzca su nombre para actualizar."
-            ref={this.inputUser}
-          />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicText">
+                <Form.Label>User name: {this.state.user}</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Introduzca su nombre para actualizar."
+                  ref={this.inputUser}
+                />
+                <Button
+                  variant="primary"
+                  type="button"
+                  onClick={handleChangeUser}
+                >
+                  Cambiar
+                </Button>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password: {this.state.password}</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Introduzca su Password para actualizar"
-            ref={this.inputPassword}
-          />
-        </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password: {this.state.password}</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Introduzca su Password para actualizar"
+                  ref={this.inputPassword}
+                />
+                <Button
+                  variant="primary"
+                  type="button"
+                  onClick={handleChangePassword}
+                >
+                  Cambiar
+                </Button>
+              </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicUrl">
-          <Form.Label>User img: {this.state.img}</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Introduzca su imagen para actualizar"
-            ref={this.inputImg}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="button" onClick={handlePerfilSetting}>
-          Submit
-        </Button>
-      </Form>
+              <Form.Group className="mb-3" controlId="formBasicUrl">
+                <Form.Label>
+                  User img:
+                  {(
+                    <img
+                      className="img-fluid"
+                      style={{ width: "3%" }}
+                      src={this.state.img}
+                    />
+                  ) || "No tiene"}
+                </Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Introduzca su imagen para actualizar"
+                  ref={this.inputImg}
+                />
+                <Button
+                  variant="primary"
+                  type="button"
+                  onClick={handleChangeImg}
+                >
+                  Cambiar
+                </Button>
+              </Form.Group>
+            </Form>
+          </div>
+        </div>
+      </div>
     );
   }
 
